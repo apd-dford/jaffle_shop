612ae68a77778d3c877a0d9681b59272079f57b8
Dylan Ford <106813318+apd-dford@users.noreply.github.com>
2023-03-07 15:46:09 -0500

    Update simulate_data_feed.py

--- notebooks/simulate_data_feed.py
+++ notebooks/simulate_data_feed.py
@@ -13,7 +13,7 @@ def slowly_add_data():
     for hour in range(10,15):
         simulate_data_feed(hour)
         time.sleep(120)
-
+        print(f"Successfully copied hour {hour} file to {target_path}")
 # COMMAND ----------
 
 slowly_add_data()
