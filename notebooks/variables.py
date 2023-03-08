69e4c4301889e674a320b5009700d20384ffbb5f
dylan.ford@aimpointdigital.com <dylan.ford@aimpointdigital.com>
2023-03-08 17:24:18 +0000

    fix

--- notebooks/variables.py
+++ notebooks/variables.py
@@ -1,5 +1,5 @@
 # Databricks notebook source
-databricks_username = "<your_username>"
+username = "<your_username>"
 # run `dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get()` to check your username
 
 catalog_name = "main"
